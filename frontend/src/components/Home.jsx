97a4b82c567540af9020af14098aec67bfcd0343
Kunal Singh <ks820177@gmail.com>
2024-04-22 08:28:38 +0530

    update home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,15 +3,15 @@ function Home() {
   return (
     <div className="bg-gradient-to-r from-black via-blue-900 to-blue-400 h-[100vh] w-[100vw]">
       <div className="flex flex-col justify-center items-center text-center p-40 text-white mx-60">
-        <p className="text-5xl font-extrabold">
+        <p className="text-2xl font-extrabold">
           Your money, your way. Instant, secure, and hassle-free
         </p>
-        <p className="text-2xl font-semibold py-10">
+        <p className="text-lg font-semibold py-10">
           Seamless payments, just a tap away. Send money, simple and secure.
         </p>
         <Link
           to="/signup"
-          className="border-transparent rounded-xl border-2 px-4 py-3 bg-black hover:bg-gray-900 transition-all duration-300 text-white font-bold text-2xl"
+          className="border-transparent rounded-lg border-2 px-3 py-3 bg-black hover:bg-gray-900 transition-all duration-300 text-white font-bold text-lg"
         >
           Get Started
         </Link>
