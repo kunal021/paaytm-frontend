bb9903ce47d9944c0122833bc866e8ca644a8b9a
Kunal Singh <ks820177@gmail.com>
2024-04-22 10:13:38 +0530

    update home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="bg-gradient-to-r from-black via-blue-900 to-blue-400 h-[100vh] w-[100vw]">
-      <div className="flex flex-col justify-center items-center text-center p-16 md:p-32 text-white h-full">
-        <p className="text-2xl font-extrabold">
+      <div className="flex flex-col justify-center items-center text-center p-10 md:p-32 text-white h-full">
+        <p className="text-lg md:text-2xl font-extrabold">
           Your money, your way. Instant, secure, and hassle-free
         </p>
-        <p className="text-lg font-semibold py-10">
+        <p className="text-sm md:text-lg font-semibold py-10">
           Seamless payments, just a tap away. Send money, simple and secure.
         </p>
         <Link
           to="/signup"
-          className="border-transparent rounded-lg border-2 px-3 py-3 bg-black hover:bg-gray-900 transition-all duration-300 text-white font-bold text-lg"
+          className="border-transparent rounded-lg border-2 px-3 py-3 bg-black hover:bg-gray-900 transition-all duration-300 text-white font-bold text-sm md:text-lg"
         >
           Get Started
         </Link>
