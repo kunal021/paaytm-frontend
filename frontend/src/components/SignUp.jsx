982946c073acf552c744a0c501b55238ef97c9cd
Kunal Singh <ks820177@gmail.com>
2024-04-22 09:56:15 +0530

    update signup

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -36,15 +36,15 @@ function SignUp() {
   return (
     <div className="flex justify-center items-center h-[100vh] w-[100vw] bg-black/30">
       <div className="flex flex-col justify-center items-center w-[50vh] rounded-lg border-transparent bg-white">
-        <h1 className="text-4xl font-black py-2 px-4">Sign Up</h1>
-        <p className="text-base font-semibold text-gray-500 text-center py-2 px-4">
+        <h1 className="text-2xl font-black py-2 px-4">Sign Up</h1>
+        <p className="text-sm font-semibold text-gray-500 text-center py-2 px-4">
           Enter your information to create an account
         </p>
         <form
           onSubmit={handleSumbit}
           className="flex flex-col justify-start items-start px-4 w-full"
         >
-          <p className="flex flex-col w-full py-2 pb-0 text-base font-semibold">
+          <p className="flex flex-col w-full py-1 pb-0 text-sm font-semibold">
             First Name{" "}
             <input
               type="text"
@@ -53,7 +53,7 @@ function SignUp() {
               className="p-2 mt-2 rounded-lg border-2"
             />
           </p>
-          <p className="flex flex-col w-full py-2 pb-0 text-base font-semibold">
+          <p className="flex flex-col w-full py-1 pb-0 text-sm font-semibold">
             Last Name{" "}
             <input
               type="text"
@@ -62,7 +62,7 @@ function SignUp() {
               className="p-2 mt-2 rounded-lg border-2"
             />
           </p>
-          <p className="flex flex-col w-full py-2 pb-0 text-base font-semibold">
+          <p className="flex flex-col w-full py-1 pb-0 text-sm font-semibold">
             Email{" "}
             <input
               type="email"
@@ -71,7 +71,7 @@ function SignUp() {
               className="p-2 mt-2 rounded-lg border-2"
             />
           </p>
-          <p className="flex flex-col w-full py-2 pb-0 text-base font-semibold">
+          <p className="flex flex-col w-full py-1 pb-0 text-sm font-semibold">
             Password{" "}
             <input
               type="password"
@@ -80,11 +80,11 @@ function SignUp() {
               className="p-2 mt-2 rounded-lg border-2"
             />
           </p>
-          <button className="flex justify-center items-center w-full p-2 mt-4 rounded-lg border-2 border-transparent bg-black hover:bg-gray-900 text-white text-base font-semibold">
+          <button className="flex justify-center items-center w-full p-2 mt-4 rounded-lg border-2 border-transparent bg-black hover:bg-gray-900 text-white text-sm font-semibold">
             Sign Up
           </button>
         </form>
-        <div className="text-base font-semibold text-gray-900 text-center py-2 px-4">
+        <div className="text-sm font-semibold text-gray-900 text-center py-2 px-4">
           Already have an account?{" "}
           <Link to="/signin" className="underline">
             Sign In
