956e2ba2e019d5618539f3084f22ec99b4dc356f
Kunal Singh <ks820177@gmail.com>
2024-04-22 10:16:37 +0530

    update home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 function Home() {
   return (
-    <div className="bg-gradient-to-r from-black via-blue-900 to-blue-400 h-[100vh] w-[100vw]">
+    <div className="bg-gradient-to-r from-black via-blue-900 to-blue-400 h-[100vh] w-[100vw] overflow-hidden">
       <div className="flex flex-col justify-center items-center text-center p-10 md:p-32 text-white h-full">
         <p className="text-lg md:text-2xl font-extrabold">
           Your money, your way. Instant, secure, and hassle-free
