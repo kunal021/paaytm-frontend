2ba35fb91a216626ffe07f8a8401420945317c40
Kunal Singh <ks820177@gmail.com>
2024-04-22 08:29:51 +0530

    update home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="bg-gradient-to-r from-black via-blue-900 to-blue-400 h-[100vh] w-[100vw]">
-      <div className="flex flex-col justify-center items-center text-center p-40 text-white mx-60">
+      <div className="flex flex-col justify-center items-center text-center p-32 text-white mx-60">
         <p className="text-2xl font-extrabold">
           Your money, your way. Instant, secure, and hassle-free
         </p>
